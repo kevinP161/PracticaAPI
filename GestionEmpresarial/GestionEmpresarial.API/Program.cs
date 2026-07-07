1547254d7b1f105c2689f275bdc9d191d0c0c95b
KevinP161 <ksantiporras12@gmail.com>
2026-07-07 13:02:10 -0500

    Clase 2

---
+++ GestionEmpresarial/GestionEmpresarial.API/Program.cs
@@ -0,0 +1,40 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+builder.Services.AddOpenApi();
+builder.Services.AddAuthorization();
+
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+
+
+//Inyecciones de dependencias
+builder.Services.AddSwaggerGen();
+
+
+ 
+
+var app = builder.Build();
+
+
+
+
+//Midleware
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
---
+++ GestionEmpresarial/GestionEmpresarial.Shared/Entities/Empresa.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestionEmpresarial.Shared.Entities
+{
+    public class Empresa
+    {
+        [Required]
+        public int Id { get; set; }
+
+        [Display(Name ="Nombre de la Empresa")]
+        [Required]
+        [MaxLength(50)]
+        public string Nombre { get; set; }
+
+    }
+}
